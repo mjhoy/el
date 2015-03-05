e83ef9c9608ed6b1a1e013528719275542a54267
Michael Hoy <mjh@mjhoy.com>
2015-03-05 17:40:13 -0500

    put setupname into a function

--- main.c
+++ main.c
@@ -29,73 +29,84 @@ const char *main_map =
       "                ";
 
 void
-intro() {
-      char *title = "🐋   L";
+setupname()
+{
+    char name[30];
+    char default_name[30];
 
-      int row, col;
+    uid_t euid;
+    struct passwd *pwd;
 
-      char name[30];
-      char default_name[30];
+    int row, col;
 
-      uid_t euid;
-      struct passwd *pwd;
+    getmaxyx(stdscr, row, col);
 
-      /* set the default name as currect effective user name */
-      euid = geteuid();         /* effective user id */
-      pwd  = getpwuid(euid);    /* passwd struct */
-      strncpy(default_name, pwd->pw_name, 29);
-      default_name[29] = '\0';
+    /* set the default name as currect effective user name */
+    euid = geteuid();         /* effective user id */
+    pwd  = getpwuid(euid);    /* passwd struct */
+    strncpy(default_name, pwd->pw_name, 29);
+    default_name[29] = '\0';
       
-      getmaxyx(stdscr, row, col);
+    clear();
+    mvprintw(
+        row-1,
+        0,
+        "what's your name? [%s] ", default_name);
 
-      attron(A_BOLD);
-      mvprintw(
-            (row / 2) - 1,
-            (col - strlen(title)) / 2,
-            "%s", title);
-      attroff(A_BOLD);
+    getnstr(name, 30);
+    if(strlen(name) == 0) {
+        setName(default_name);
+    } else {
+        setName(name);
+    }
 
-      mvprintw(
-            row-1,
-            0,
-            "hit a key to continue ");
+    clear();
+    mvprintw(
+        row-1,
+        0,
+        "hello, %s! (hit a key to continue)",
+        getName());
+    refresh();
+    getch();
+}
 
-      refresh();
-      getch();
+void
+intro() {
+    char *title = "🐋   L";
 
-      clear();
-      mvprintw(
-            row-1,
-            0,
-            "what's your name? [%s] ", default_name);
+    int row, col;
 
-      getnstr(name, 30);
-      if(strlen(name) == 0) {
-          setName(default_name);
-      } else {
-          setName(name);
-      }
-      clear();
-      mvprintw(
-          row-1,
-          0,
-          "hello, %s! (hit a key to continue)",
-          getName());
-      refresh();
-      getch();
+    getmaxyx(stdscr, row, col);
+
+    attron(A_BOLD);
+    mvprintw(
+        (row / 2) - 1,
+        (col - strlen(title)) / 2,
+        "%s", title);
+    attroff(A_BOLD);
+
+    mvprintw(
+        row-1,
+        0,
+        "hit a key to continue ");
+
+    refresh();
+    getch();
+
+    setupname();
 }
 
 int
 main() {
-      init_log();
-      setlocale(LC_ALL, "en_US.UTF-8");
-      initscr();
+    init_log();
+    setlocale(LC_ALL, "en_US.UTF-8");
+    initscr();
 
-      intro();
+    intro();
 
-      run_level_1();
+    run_level_1();
 
-      endwin();
-      end_log();
-      return 0;
+    endwin();
+    end_log();
+    return 0;
 }

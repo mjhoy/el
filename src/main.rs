dfde81c44d47e3dd369c0074534f116872738335
Michael Hoy <mjh@mjhoy.com>
2016-09-24 11:44:27 -0500

    starting over

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,16 @@
 #[macro_use]
-extern crate log;
 extern crate ncurses;
-extern crate chrono;
-
-mod my_log;
-mod map;
-mod level_1;
-mod doodad;
-mod wall;
-mod door;
-mod render;
 
 use ncurses::*;
-use chrono::*;
-use render::*;
-
-use level_1::*;
 
 fn main() {
-    my_log::init().unwrap();
-    info!("Starting up");
-
     let locale_conf = LcCategory::all;
     setlocale(locale_conf, "en_US.UTF-8"); // if your locale is like mine(zh_CN.UTF-8)
 
     initscr();
     start_color();
-    init_colors();
+
     intro();
-
-    run_level_1();
-
     endwin();
 }
 
@@ -60,30 +40,8 @@ fn intro() {
     getch();
 
     setupname();
-
-    tellmeaboutthedate();
 }
 
-fn tellmeaboutthedate() {
-    let mut row = 0;
-    let mut col = 0;
-
-    let now: DateTime<UTC> = UTC::now();
-
-    getmaxyx(stdscr, &mut row, &mut col);
-    clear();
-
-    mvprintw(
-        row-3,
-        0,
-        &format!("just so you know it has been {} days since the year began.", now.ordinal()));
-    mvprintw(
-        row-2,
-        0,
-        "(hit a key to continue)");
-    refresh();
-    getch();
-}
 
 fn setupname() {
     let default_name = "mikey";
